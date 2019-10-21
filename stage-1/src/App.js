2190f77d137e4b50a78f684f2ec09da31e64b2df
Stephen <hubbard_stephen@yaho.com>
2019-10-21 16:19:25 -0600

    done with all

--- stage-1/src/App.js
+++ stage-1/src/App.js
@@ -3,6 +3,8 @@ import StoreFront from "./Components/StoreFront/StoreFront";
 import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
 
+import axios from "axios";
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -54,9 +56,13 @@ class App extends Component {
         <NavBar navigate={this.navigate} />
         <div className="main-container">
           {showCart ? (
-            <ShoppingCart cart={cart} />
+            <ShoppingCart cart={this.state.cart} 
+                          removeFromCart={this.removeFromCart}
+            />
           ) : (
-            <StoreFront products={products} />
+            <StoreFront products={products} 
+                        addToCart={this.addToCart}
+            />
           )}
         </div>
       </div>
